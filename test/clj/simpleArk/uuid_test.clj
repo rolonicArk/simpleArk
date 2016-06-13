fae0fde074497f43c080e95bdf3662e05cab9746
Bill La Forge <laforge49@gmail.com>
2016-06-13 17:16:50 -0400

    real tests for uuid now

---
+++ test/clj/simpleArk/uuid_test.clj
@@ -0,0 +1,26 @@
+(ns simpleArk.uuid-test
+  (:require [clojure.test :refer :all]
+            [clj-uuid :as uuid]))
+
+(set! *warn-on-reflection* true)
+
+(def uuid0 (java.util.UUID/randomUUID))
+
+(def uuid1 (uuid/v1))
+(def uuid2 (uuid/v1))
+(def uuid3 (uuid/v1))
+(def uuid4 (uuid/v1))
+(def uuid5 (uuid/v1))
+
+(deftest uuids
+  (is (= (uuid/get-version uuid0) 4))
+  (is (nil? (uuid/get-timestamp uuid0)))
+
+  (is (= (uuid/get-version uuid1) 1))
+  (is (uuid/get-timestamp uuid1))
+
+  (is (= (compare uuid1 uuid2) -1))
+  (is (= (compare uuid2 uuid3) -1))
+
+  (is (uuid? uuid1))
+  (is (uuid/uuid? uuid0)))

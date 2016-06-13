db528d57c6d79ce75b664e9099ed7632610dc2e8
Bill La Forge <laforge49@gmail.com>
2016-06-13 10:21:43 -0400

    generated project.clj file

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject
+  boot-project
+  "0.0.0-SNAPSHOT"
+  :dependencies
+  [[org.clojure/clojure "1.9.0-alpha5" :scope "provided"]
+   [adzerk/boot-test "1.1.1" :scope "test"]]
+  :source-paths
+  ["test/clj" "src/clj"])

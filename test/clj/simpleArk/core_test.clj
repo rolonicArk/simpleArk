4f22fb2cd263ba9a86391d51fea05da63a33929e
Bill La Forge <laforge49@gmail.com>
2016-06-13 14:36:49 -0400

    added test of type 1 uuid

--- test/clj/simpleArk/core_test.clj
+++ test/clj/simpleArk/core_test.clj
@@ -1,1 +1,17 @@
-(ns simpleArk.core-test)
+(ns simpleArk.core-test
+  (:require [clj-uuid :as uuid]))
+
+(def uuid1 (uuid/v1))
+(def uuid2 (uuid/v1))
+(def uuid3 (uuid/v1))
+(def uuid4 (uuid/v1))
+(def uuid5 (uuid/v1))
+
+(println "uuid1" uuid1 (uuid/get-timestamp uuid1))
+(println "uuid2" uuid2 (uuid/get-timestamp uuid2))
+(println "uuid3" uuid3 (uuid/get-timestamp uuid3))
+(println "uuid4" uuid3 (uuid/get-timestamp uuid4))
+(println "uuid5" uuid3 (uuid/get-timestamp uuid5))
+
+(println (.compareTo uuid1 uuid2))
+(println (.compareTo uuid2 uuid3))

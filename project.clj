71c9211862fc0a38c409fe3204e734a356578da5
Bill La Forge <laforge49@gmail.com>
2016-06-13 20:25:36 -0400

    regenerated project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   "0.0.0-SNAPSHOT"
   :dependencies
   [[org.clojure/clojure "1.9.0-alpha5" :scope "provided"]
+   [danlentz/clj-uuid "0.1.6"]
    [adzerk/boot-test "1.1.1" :scope "test"]]
   :source-paths
   ["test/clj" "src/clj"])

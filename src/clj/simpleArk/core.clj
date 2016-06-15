1ffece2a1bc93298b0ec1433e9f89a7b07084dc3
Bill La Forge <laforge49@gmail.com>
2016-06-15 10:26:51 -0400

    api tweak

--- src/clj/simpleArk/core.clj
+++ src/clj/simpleArk/core.clj
@@ -12,7 +12,7 @@
 
 (defrecord Ark [get-rolon get-journal-entry-uuids get-rolon-uuids])
 
-(defrecord Rolon [rolon-uuid get-rolon-values])
+(defrecord Rolon [rolon-uuid get-journal-entry-uuids get-rolon-value])
 
 (defrecord Rolon-value [rolon journal-entry-uuid
                         get-property-keys get-property-value get-property-journal-entry-uuid])
@@ -23,9 +23,10 @@
   ((:get-rolon ark) uuid))
 
 (defn get-journal-entry-uuids
-  "returns a sorted set of all the journal entry rolon uuids"
-  [ark]
-  ((:get-journal-entry-uuids ark)))
+  "returns a sorted set of all the journal entry rolon uuids,
+  where rec is an ark or a rolon"
+  [rec]
+  ((:get-journal-entry-uuids rec)))
 
 (defn get-rolon-uuids
   "returns a sorted set of all rolon uuids"
@@ -37,10 +38,10 @@
   [rolon]
   (:rolon-uuid rolon))
 
-(defn get-rolon-values
-  "returns a map of the rolon values keyed by journal entry uuid"
-  [rolon]
-  ((:get-rolon-values rolon)))
+(defn get-rolon-value
+  "returns the rolon value keyed by the journal entry uuid"
+  [rolon journal-entry-uuid]
+  ((:get-rolon-value rolon) journal-entry-uuid))
 
 (defn get-journal-entry-uuid
   "returns the type-1 uuid of the journal entry rolon which created this rolon value"
@@ -55,17 +56,18 @@
 (defn get-property-value
   "returns the value of a property, or nil"
   [rolon-value property-name]
-  ((:get-property-value rolon-value property-name)))
+  ((:get-property-value rolon-value) property-name))
 
 (defn get-property-journal-entry-uuid
   "returns the type 1 uuid of the journal entry rolon which changed the property to the given value"
-  [rolon-value]
-  ((:get-property-journal-entry-uuid rolon-value)))
+  [rolon-value property-name]
+  ((:get-property-journal-entry-uuid rolon-value) property-name))
 
 (defn get-updated-rolon-uuids
   "returns the uuids of the rolons updated by a journal-entry rolon"
   [journal-entry]
-  (let [current-value (last (get-rolon-values journal-entry))]
+  (let [last-journal-entry-uuid (last (get-journal-entry-uuids journal-entry))
+        current-value (get-rolon-value journal-entry last-journal-entry-uuid)]
     (get-property-value current-value :journal-entry:updated-rolon-uuids)))
 
 (defn get-value-rolon
@@ -76,8 +78,8 @@
 (defn get-previous-value
   "returns the previous rolon value for the same rolon, or nil"
   [rolon-value]
-  (let [rolon (get-value-rolon rolon-value)
-        rolon-values (get-rolon-values rolon)
-        journal-entry-uuid (get-journal-entry-uuid rolon-value)
-        previous-values (rsubseq rolon-values < journal-entry-uuid)]
-    (val (first previous-values))))
+  (let [journal-entry-uuid (get-journal-entry-uuid rolon-value)
+        rolon (get-value-rolon rolon-value)
+        journal-entry-uuids (get-journal-entry-uuids rolon)
+        previous-journal-entry-uuids (rsubseq journal-entry-uuids < journal-entry-uuid)]
+    (first previous-journal-entry-uuids)))

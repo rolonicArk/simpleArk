8ae726e2191e905c46774ae14c5ec2cffd50bf9e
Bill La Forge <laforge49@gmail.com>
2016-06-15 06:57:09 -0400

    capitalized records and protocols

--- src/clj/simpleArk/core.clj
+++ src/clj/simpleArk/core.clj
@@ -1,67 +1,69 @@
 (ns simpleArk.core
   (:require [clj-uuid :as uuid]))
 
-(defrecord ark [])
+(defrecord Ark [get-rolon get-journal-entry-uuids])
 
-(defrecord rolon [])
+(defrecord Rolon [get-journal-entry-uuids get-rolon-uuids get-rolon-uuid])
 
-(defrecord journal-entry [])
+(defrecord Journal-entry [get-journal-entry-uuids get-rolon-uuids get-rolon-uuid
+                          get-rolon-values])
 
-(defrecord rolon-value [])
+(defrecord Rolon-value [get-rolon-uuid get-journal-entry-uuid get-previous-value
+                        get-property-keys get-property-value get-property-journal-entry-uuid])
 
 (defn get-rolon
   "returns the rolon identified by the uuid, or nil"
   [ark uuid]
-  ((:ark:get-rolon ark) uuid))
+  ((:get-rolon ark) uuid))
 
 (defn get-journal-entry-uuids
   "returns a sorted set of all the journal entry rolon uuids,
   where rec is an ark or a rolon"
   [rec]
-  ((:ark:get-journal-entry-uuids rec)))
+  ((:get-journal-entry-uuids rec)))
 
 (defn get-rolon-uuids
   "returns a sorted set of all rolon uuids"
   [ark]
-  ((:ark:get-rolon-uuids ark)))
+  ((:get-rolon-uuids ark)))
 
 (defn get-rolon-uuid
   "returns the uuid of the rolon,
   where rec is a rolon or rolon-value"
   [rec]
-  (:ark:rolon-uuid rec))
+  (:rolon-uuid rec))
 
 (defn get-rolon-values
   "returns a set of the rolon values created by this journal-entry rolon"
   [journal-entry]
-  ((:ark:get-rolon-values journal-entry)))
+  ((:get-rolon-values journal-entry)))
 
 (defn get-journal-entry-uuid
   "returns the type-1 uuid of the journal entry rolon which created this rolon value"
   [rolon-value]
-  (:ark:journal-entry-uuid rolon-value))
+  (:journal-entry-uuid rolon-value))
 
 (defn get-previous-value
   [rolon-value]
   "returns the previous rolon value for the same rolon, or nil"
-  ((:ark:get-previous-value rolon-value)))
+  ((:get-previous-value rolon-value)))
 
 (defn get-property-keys
   [rolon-value]
   "returns a sorted set of the keys of all the properties assigned to this or a previous rolon value"
-  ((:ark:get-property-keys rolon-value)))
+  ((:get-property-keys rolon-value)))
 
 (defn get-property-value
   [rolon-value]
   "returns the value of a property, or nil"
-  ((:ark:get-property-value rolon-value)))
+  ((:get-property-value rolon-value)))
 
 (defn get-property-journal-entry-uuid
   [rolon-value]
   "returns the type 1 uuid of the journal entry rolon which changed the property to the given value"
-  ((:ark:get-property-journal-entry-uuid rolon-value)))
+  ((:get-property-journal-entry-uuid rolon-value)))
 
-(defprotocol ark-db
+(defprotocol Ark-db
   (get-ark [this]
     "returns the current value of the ark")
   (register-transaction! [this transaction-name p]

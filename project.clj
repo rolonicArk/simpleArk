6b70f715b3884310c955a1a036398be0549ecb40
Bill La Forge <laforge49@gmail.com>
2016-06-15 18:09:46 -0400

    alpha7

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   boot-project
   "0.0.0-SNAPSHOT"
   :dependencies
-  [[org.clojure/clojure "1.9.0-alpha6" :scope "provided"]
+  [[org.clojure/clojure "1.9.0-alpha7" :scope "provided"]
    [danlentz/clj-uuid "0.1.6"]
    [adzerk/boot-test "1.1.1" :scope "test"]]
   :source-paths

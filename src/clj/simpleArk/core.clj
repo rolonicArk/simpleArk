a0853be74c800b58124d9e68966880330b9767ac
Bill La Forge <laforge49@gmail.com>
2016-06-14 21:05:00 -0400

    defined records, replaced ark protocol

--- src/clj/simpleArk/core.clj
+++ src/clj/simpleArk/core.clj
@@ -1,25 +1,40 @@
 (ns simpleArk.core
   (:require [clj-uuid :as uuid]))
 
-(defprotocol ark
-  (get-rolon [this uuid]
-    "returns the rolon identified by the uuid, or nil")
-  (get-journal-entry-uuids [this]
-    "returns a sorted set of all the journal entry rolon uuids")
-  (get-rolon-uuids [this]
-    "returns a sorted set of all rolon uuids"))
+(defrecord ark [])
 
-(defprotocol rolon
+(defrecord rolon [])
+
+(defrecord journal-entry [])
+
+(defrecord rolon-value [])
+
+(defn get-rolon
+  "returns the rolon identified by the uuid, or nil"
+  [ark uuid]
+  ((::get-rolon ark) uuid))
+
+(defn get-journal-entry-uuids
+  "returns a sorted set of all the journal entry rolon uuids"
+  [ark]
+  ((::get-journal-entry-uuids ark)))
+
+(defn get-rolon-uuids
+  "returns a sorted set of all rolon uuids"
+  [ark]
+  ((::get-rolon-uuids ark)))
+
+(defprotocol rolon-protocol
   (get-rolon-uuid [this]
     "returns the uuid of the rolon")
   (get-journal-entry-uuids-for-rolon [this]
     "returns a sorted set of the uuids of all the journal entries which have updated this rolon"))
 
-(defprotocol journal-entry-rolon
+(defprotocol journal-entry-rolon-protocol
   (get-updated-rolon-values [this]
     "returns a sorted set of the rolon values created by this journal entry rolon"))
 
-(defprotocol rolon-value
+(defprotocol rolon-value-protocol
   (get-rolon-uuid-for-value [this]
     "returns the uuid of the rolon")
   (get-journal-entry-uuid [this]

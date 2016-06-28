8f77b7c780c9e1c2609f7b1889a4ef9c5b4614c3
Bill La Forge <laforge49@gmail.com>
2016-06-28 09:25:04 -0400

    added time navigation tests

--- test/clj/simpleArk/core_test.clj
+++ test/clj/simpleArk/core_test.clj
@@ -51,18 +51,18 @@
 (defn test0
   "tests that even work with impl0"
   [ark-db]
+  (register-transaction! ark-db ::hello-world hello-world)
   (register-transaction! ark-db ::make-rolon-transaction make-rolon-transaction)
   (register-transaction! ark-db ::destroy-rolon-transaction destroy-rolon-transaction)
 
   (println)
   (println ">>>>>>>>>>>> hello-world")
   (println)
-  (register-transaction! ark-db ::hello-world hello-world)
   (let [je-uuid (process-transaction! ark-db ::hello-world "Fred")
-        ark (get-ark ark-db)
-        je-properties (get-current-property-values ark je-uuid)]
-    (println :je-uuid je-uuid)
-    (println :transaction-properties je-properties)
+        ark (get-ark ark-db)]
+    (def hello-je-uuid je-uuid)
+    (println :hello-je-uuid hello-je-uuid)
+    (println :je-properties (get-current-property-values ark je-uuid))
     )
 
   (println)
@@ -71,14 +71,13 @@
   (let [je-uuid (process-transaction! ark-db ::make-rolon-transaction
                                       (prn-str {:descriptor/age 8
                                                 :classifier/name "Bob"}))
-        ark (get-ark ark-db)
-        je-properties (get-current-property-values ark je-uuid)
-        bob-uuid (name-lookup ark "Bob")
-        bob-properties (get-current-property-values ark bob-uuid)]
-    (println :je-uuid je-uuid)
-    (println :transaction-properties je-properties)
+        ark (get-ark ark-db)]
+    (def make-bob-je-uuid je-uuid)
+    (def bob-uuid (name-lookup ark "Bob"))
+    (println :make-bob-je-uuid make-bob-je-uuid)
+    (println :je-properties (get-current-property-values ark je-uuid))
     (println :bob-uuid bob-uuid)
-    (println :bob-properties bob-properties)
+    (println :bob-properties (get-current-property-values ark bob-uuid))
     )
 
   (println)
@@ -88,36 +87,41 @@
                                       (prn-str {:descriptor/age 10
                                                 :classifier/name "Sam"
                                                 :classifier/headline "I hate green eggs and ham!"}))
-        ark (get-ark ark-db)
-        je-properties (get-current-property-values ark je-uuid)
-        sam-uuid (name-lookup ark "Sam")
-        sam-properties (get-current-property-values ark sam-uuid)]
-    (println :je-uuid je-uuid)
-    (println :transaction-properties je-properties)
+        ark (get-ark ark-db)]
+    (def make-sam-je-uuid je-uuid)
+    (def sam-uuid (name-lookup ark "Sam"))
+    (println :make-sam-je-uuid make-sam-je-uuid)
+    ;(println :je-properties (get-current-property-values ark je-uuid))
     (println :sam-uuid sam-uuid)
-    (println :sam-properties sam-properties)
+    (println :sam-properties (get-current-property-values ark sam-uuid))
     )
 
   (println)
   (println ">>>>>>>>>>>> destroy-bob")
   (println)
-  (let [ark (get-ark ark-db)
-        bob-uuid (name-lookup ark "Bob")
-        je-uuid (process-transaction! ark-db ::destroy-rolon-transaction
+  (let [je-uuid (process-transaction! ark-db ::destroy-rolon-transaction
                                       (prn-str bob-uuid))
-        ark (get-ark ark-db)
-        je-properties (get-current-property-values ark je-uuid)
-        bob-properties (get-current-property-values ark bob-uuid)]
-    (println :je-uuid je-uuid)
-    (println :transaction-properties je-properties)
-    (println :bob-uuid bob-uuid)
-    (println :bob-properties bob-properties)
+        ark (get-ark ark-db)]
+    (def destroy-bob-je-uuid je-uuid)
+    ;(println :destroy-bob-je-uuid destroy-bob-je-uuid)
+    ;(println :je-properties (get-current-property-values ark je-uuid))
+    ;(println :bob-uuid bob-uuid)
+    (println :bob-properties (get-current-property-values ark bob-uuid))
+    (println :lookup-bob (name-lookup ark "Bob"))
     )
 
   (println)
-  (println ">>>>>>>>>>>> ark")
+  (println ">>>>>>>>>> select time: make-bob-je-uuid")
   (println)
-  (println (get-ark ark-db))
+  (let [ark (get-ark ark-db)
+        ark (select-time ark make-bob-je-uuid)]
+    (println :bob-properties (get-current-property-values ark bob-uuid))
+    (println :lookup-bob (name-lookup ark "Bob")))
+
+  (println)
+  ;(println ">>>>>>>>>>>> ark")
+  (println)
+  ;(println (get-ark ark-db))
   )
 
 (deftest arks

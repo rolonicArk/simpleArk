93abc14cc1fcf1fe441ecc454a4d90f005f3aaff
Bill La Forge <laforge49@gmail.com>
2016-07-27 11:36:52 -0400

    tlog1 draft

---
+++ src/clj/simpleArk/tlog1.clj
@@ -0,0 +1,27 @@
+(ns simpleArk.tlog1
+  (:require [clojure.core.async :as async]
+            [simpleArk.log :as log]
+            [simpleArk.core :as ark]))
+
+(set! *warn-on-reflection* true)
+
+(defn add-tran!
+  [m je-uuid transaction-name s rsp-chan ark]
+  (swap! (::va m) conj [je-uuid transaction-name s])
+  (log/info! m :transaction transaction-name s)
+  (ark/publish m ark [[rsp-chan ark]]))
+
+(defn tran-seq
+  [m position]
+  (def vi (volatile! position))
+  (map (fn [v]
+         (conj v (vswap! vi inc)))
+       (subvec @(::va m) position)))
+
+(defn builder
+  []
+  (fn [m]
+    (-> m
+        (assoc ::va (atom []))
+        (assoc :tran-logger/add-tran add-tran!)
+        (assoc :tran-logger/tran-seq tran-seq))))

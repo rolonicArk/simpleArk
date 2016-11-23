64ff7d4e48f7c6fad0404d0a320a4e8f70f9ac1b
Bill La Forge <laforge49@gmail.com>
2016-11-22 19:55:34 -0500

    reworked welcome client

---
+++ src/welcome/client.cljs
@@ -0,0 +1,13 @@
+(ns welcome.client
+  (:require
+    [hoplon.core :as h]
+    [tiples.login :as login]))
+
+(def do-welcome
+  (h/div :id "header"
+       :style "background-color:#f8f8f0"
+       (login/tabs-div)
+       (h/h2 (h/text "Hello ~{(:full-name (:welcome login/user-data))}."))))
+
+(defmethod login/add-element :welcome [_]
+  (do-welcome))

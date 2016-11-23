0a0a76b974d04d386b89d1f56dd42858d082c459
Bill La Forge <laforge49@gmail.com>
2016-11-22 19:57:43 -0500

    reformat

--- src/welcome/client.cljs
+++ src/welcome/client.cljs
@@ -5,9 +5,9 @@
 
 (def do-welcome
   (h/div :id "header"
-       :style "background-color:#f8f8f0"
-       (login/tabs-div)
-       (h/h2 (h/text "Hello ~{(:full-name (:welcome login/user-data))}."))))
+         :style "background-color:#f8f8f0"
+         (login/tabs-div)
+         (h/h2 (h/text "Hello ~{(:full-name (:welcome login/user-data))}."))))
 
 (defmethod login/add-element :welcome [_]
   (do-welcome))

b7de5457b0c30b7b9a14b42db31c151f09f80fdd
Bill La Forge <laforge49@gmail.com>
2016-12-20 10:27:56 -0500

    started the list changes over time command

--- src/console/micro_property_commands.cljs
+++ src/console/micro_property_commands.cljs
@@ -2,13 +2,36 @@
   (:require
     [hoplon.core :as h]
     [javelin.core :as j]
-    [console.client :as client]))
+    [console.client :as client]
+    [simpleArk.uuid :as suuid]
+    [simpleArk.arkRecord :as arkRecord]))
 
 (defn list-changes
   [ark-record]
   (client/add-prompt)
   (client/add-history! ">")
-  (client/add-history! "list changes over time\n" client/command-prefix-style)
+  (client/add-history! "list changes over time for " client/command-prefix-style)
+  (let [path @client/selected-microp
+        uuid (suuid/create-uuid @client/selected-rolon)
+        changes (arkRecord/get-changes-by-property ark-record uuid path)]
+    (client/history-path! ark-record path)
+    (client/add-history! " in ")
+    (client/add-history!
+      (client/pretty-uuid ark-record uuid)
+      (client/clickable-styles uuid)
+      client/uuid-click
+      @client/selected-rolon)
+    (client/add-history! "\n")
+    (client/clear-output!)
+    (client/add-output! "changes over time for ")
+    (client/output-path! ark-record path)
+    (client/add-output! " in ")
+    (client/add-output!
+      (client/pretty-uuid ark-record uuid)
+      (client/clickable-styles uuid)
+      client/uuid-click
+      @client/selected-rolon))
+  (client/add-output! "\n\n")
   )
 
 (defn do-micro-property-commands

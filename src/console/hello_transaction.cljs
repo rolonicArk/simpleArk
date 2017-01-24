e96d5aa07410a9a18a2fa817d1348ba303daf698
Bill La Forge <laforge49@gmail.com>
2017-01-24 10:26:39 -0500

    parameterized Hello world!

--- src/console/hello_transaction.cljs
+++ src/console/hello_transaction.cljs
@@ -14,19 +14,33 @@
         (builder/build-je-property
           [:index/headline] "Just for fun!"))))
 
+(def aname (j/cell ""))
+
+(defn get-aname []
+  (if (= "" @aname)
+    "world"
+    @aname))
+
 (defn do-hello
   []
   (h/div
     :css {:display "none"}
     :toggle (j/cell= (= "Hello world!" client/form-name))
-
-    (h/button
-      :style "background-color:MistyRose"
-      :click (fn []
-               (reset! client/display-mode 0)
-               (client/add-prompt)
-               (client/add-history! ">")
-               (client/add-history! "Hello Fred transaction\n" client/command-prefix-style)
-               (hello-transaction "Fred"))
-      "Submit")))
+    (h/form
+      :submit (fn []
+                (reset! client/display-mode 0)
+                (client/add-prompt)
+                (client/add-history! ">")
+                (client/add-history! (str "Hello " (get-aname) "! transaction\n") client/command-prefix-style)
+                (hello-transaction (get-aname)))
+      (h/div
+        (h/label "Hello ")
+        (h/input :type "text"
+                 :value aname
+                 :keyup #(reset! aname @%))
+        (h/label "!"))
+      (h/button
+        :style "background-color:MistyRose"
+        :type "submit"
+        "Submit"))))
 

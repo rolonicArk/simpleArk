a9611371486146449052e0d72615fd16ad46d692
Bill La Forge <laforge49@gmail.com>
2017-02-09 08:33:00 -0500

    finished composition-delete-rolon

--- src/console/composition_delete_rolon.cljs
+++ src/console/composition_delete_rolon.cljs
@@ -7,7 +7,25 @@
 
 (def delete-uuid (j/cell ""))
 
+(defn valid
+  [uuid]
+  (client/valid-parameter uuid))
+
 (defn do-delete-rolon
   []
   (h/form
-    ))
+    :submit (fn []
+              (if (valid @delete-uuid)
+                (swap! client/actions builder/build-delete-rolon
+                       (keyword "local" @delete-uuid)))
+              (client/display-composition))
+    (h/label "Delete Rolon :local/")
+    (h/input :type "text"
+             :css {:background-color "LightYellow"}
+             :value delete-uuid
+             :keyup #(reset! delete-uuid @%))
+    (h/button
+      :css {:display "none" :background-color "MistyRose"}
+      :toggle (j/cell= (valid delete-uuid))
+      :type "submit"
+      "OK")))
